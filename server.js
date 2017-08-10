2591c6989914f5a652a85d0c9bea214becebdbbe
Kritsada Dechawantana <kritsada@perfenterprise.com>
2017-08-10 08:57:38 +0700

    Init proj

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+let data = require('./data')
+
+app.use(bodyParser.json())
+
+//วิธีกำหนด routing หรือ API endpoint
+app.get('/', function (req, res) {
+  res.send('API Started')
+})
+
+app.get('/accounts', function (req, res) {
+  res.json(data.accounts)
+})
+
+app.get('/account/:id', function (req, res) {
+  let account = data.accounts.filter(acc => acc.account_id === req.params.id);
+  res.json(account)
+})
+
+app.post('/account/add', function (req, res) {
+  data.accounts.push(req.body)
+  res.json(req.body)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
