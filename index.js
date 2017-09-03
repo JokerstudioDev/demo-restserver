425c7bf7492c89faaebe03e78741fa661b73403f
Kritsada Dechawantana <kritsada@perfenterprise.com>
2017-09-03 19:27:58 +0700

    fix port

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.post('/account/add', function (req, res) {
   res.json(req.body)
 })
 
-app.listen(3000, function () {
+var port = process.env.PORT || 1337;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
